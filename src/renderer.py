de510892e1be188cab466c229911016c897a1c2b
TimonKnigge <timonknigge@live.nl>
2014-05-30 17:23:25 +0200

    Finished renderer, added a second sample.

--- src/renderer.py
+++ src/renderer.py
@@ -2,12 +2,13 @@ from PIL import Image, ImageDraw
 from cmath import *
 
 
-def rendertoimage(equation, blowup, _xinterval, _yinterval, iterations, _resolution, filename):
-    # save globally so they can be used by renderer.pixeltocomplex
-    global xinterval, yinterval, resolution
+def rendertoimage(equation, blowup, _xinterval, _yinterval, _iterations, _resolution, filename):
+    # save globally so they can be used by renderer.pixeltocomplex and renderer.iterationstopixel
+    global xinterval, yinterval, resolution, iterations
     xinterval = _xinterval
     yinterval = _yinterval
     resolution = _resolution
+    iterations = _iterations
 
     image = Image.new("RGB", resolution)
 
@@ -18,12 +19,8 @@ def rendertoimage(equation, blowup, _xinterval, _yinterval, iterations, _resolut
             while (i < iterations) and (abs(z) < blowup):
                 z = eval(equation)
                 i += 1
-            v = int((i / iterations) * 255)
-            image.putpixel((x, y), (v, v, v))
 
-    # z = 2
-    # print(eval(equation))
-    #image.putpixel((1, 2), (255, 0, 255))
+            image.putpixel((x, y), iterationstopixel(i))
 
     image.save(filename, "PNG")
 
@@ -33,3 +30,11 @@ def pixeltocomplex(xpos, ypos):
     re = (xpos / resolution[0]) * (xinterval[1] - xinterval[0]) + xinterval[0]
     im = (ypos / resolution[1]) * (yinterval[1] - yinterval[0]) + yinterval[0]
     return complex(re, im)
+
+
+def iterationstopixel(i):
+    delta = i / iterations
+    r = min(3 * delta, 1) * 256
+    g = max(min(3 * delta - 1, 1), 0) * 256
+    b = max(min(3 * delta - 2, 1), 0) * 256
+    return int(r), int(g), int(b)
